be0f9a12ef402826198c470dff849a4655e017fc
KAMOHARA Jun <23qkamohalalalajun@gmail.com>
2020-06-02 15:51:49 +0900

    テストをcで書き直す

---
+++ tests/test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cargo build
+./target/debug/rnc <(cat tests/tests.c) > bind/tmp.s
+CMD="gcc -static -o tmp tmp.s; ./tmp"
+docker run --rm --mount type=bind,src=$PWD/bind,dst=/home/user --workdir /home/user compilerbook /bin/bash -c "$CMD"
